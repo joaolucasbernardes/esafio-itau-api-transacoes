9c12f49cf9a74593cb909feea764be0b68cf6d79
Joao Lucas <joaolucasbernardes@gmail.com>
2025-07-15 19:06:38 -0300

    Commit Inicial: Configuração do projeto Spring Boot

---
+++ src/main/java/com/itau/desafio/api_transacoes/ApiTransacoesApplication.java
@@ -0,0 +1,13 @@
+package com.itau.desafio.api_transacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTransacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTransacoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itau/desafio/api_transacoes/ApiTransacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itau.desafio.api_transacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTransacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
